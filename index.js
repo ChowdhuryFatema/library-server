92127e015f7c813a64a1daea9b8be6809b629704
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-05-13 15:33:06 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -7,9 +9,17 @@ const app = express();
 const port = process.env.PORT || 5000; 
 
 // middleware
-app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    // 'https://car-doctor-791f6.web.app',
+    // 'https://car-doctor-791f6.firebaseapp.com',
+  ],
+  credentials: true
+}));
 
+app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxai2xc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,17 +33,94 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+// middleware 
+const logger = (req, res, next) => {
+  console.log('log: info', req.method, req.url);
+  next()
+}
+
+
+const verifyToken = (req, res, next) => {
+
+  const token = req?.cookies?.token;
+  console.log('token in the middleware', token);
+
+  // no token available 
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next()
+  })
+
+}
+
+const cookeOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production' ? true : false,
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const booksCollection = client.db("libraryDB").collection("books");
+    const borrowedBooksCollection = client.db("libraryDB").collection("borrowedBooks");
 
-    app.get('/books', async(req, res) => {
+
+ // auth related api 
+
+ app.post('/jwt', async(req, res) => {
+  const user = req.body;
+  console.log(user)
+
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , { expiresIn: '1h' })
+
+  res.cookie('token', token, cookeOption)
+  .send({success: true})
+
+})
+
+app.post('/logout', async(req, res) => {
+  const user = req.body;
+  console.log('log out', user);
+  res.clearCookie('token', {maxAge: 0}).send({success: true})
+})
+
+
+
+    // book related api
+
+
+    app.get('/books', logger, verifyToken, async(req, res) => {
+
+      // if(req.user?.email !== req.query.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+
+
+      // let query = {};
+      // if(req.query?.email){
+      //   query = {email: req.query.email}
+      // }
+
         const result = await booksCollection.find().toArray();
         res.send(result) 
     })
+
+
+
     app.get('/book/:category', async(req, res) => {
       const cat = req.params.category;
       console.log(cat);
@@ -53,10 +140,11 @@ async function run() {
     })
 
     app.post('/books', async(req, res) => {
-        console.log(req.body);
         const result = await booksCollection.insertOne(req.body);
         res.send(result);
     })
+
+  
 
 
     app.put ( '/books/:id', async(req, res) => {
@@ -81,6 +169,30 @@ async function run() {
       res.send(result);
 
     })
+
+
+    // borrowed Book 
+
+    app.get('/borrowedBooks', async(req, res) => {
+      const result = await borrowedBooksCollection.find().toArray();
+      res.send(result) 
+  })
+
+
+    app.post('/borrowedBooks', async(req, res) => {
+      console.log(req.body);
+      const result = await borrowedBooksCollection.insertOne(req.body);
+      res.send(result);
+    })
+
+    app.delete('/borrowedBooks/:id', async(req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const result = await borrowedBooksCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
 
 
